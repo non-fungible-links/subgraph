98ce7de9876e2b00a14c63eda8a40c8f7b34a45f
Ajand Mardalizad <aseedgeek@gmail.com>
2023-10-22 16:22:36 +0400

    basic subgraph added

--- src/nflinks.ts
+++ src/nflinks.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   nflinks,
   ApprovalForAll,
@@ -12,40 +12,37 @@ import {
   TransferSingle,
   URI,
   Unpaused,
-  UserRegistered
-} from "../generated/nflinks/nflinks"
-import { ExampleEntity } from "../generated/schema"
+  UserRegistered,
+} from "../generated/nflinks/nflinks";
+import { NFT, Link } from "../generated/schema";
 
 export function handleApprovalForAll(event: ApprovalForAll): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
-
+  //let entity = ExampleEntity.load(event.transaction.from);
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.account = event.params.account
-  entity.operator = event.params.operator
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
+  //if (!entity) {
+  //  entity = new ExampleEntity(event.transaction.from);
+  //
+  //  // Entity fields can be set using simple assignments
+  //  entity.count = BigInt.fromI32(0);
+  //}
+  //
+  //// BigInt and BigDecimal math are supported
+  //entity.count = entity.count + BigInt.fromI32(1);
+  //
+  //// Entity fields can be set based on event parameters
+  //entity.account = event.params.account;
+  //entity.operator = event.params.operator;
+  //
+  //// Entities can be written to the store with `.save()`
+  //entity.save();
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
   // `new Entity(...)`, set the fields that should be updated and save the
   // entity back to the store. Fields that were not set or unset remain
   // unchanged, allowing for partial updates to be applied.
-
   // It is also possible to access smart contracts from mappings. For
   // example, the contract that has emitted the event can be connected to
   // with:
@@ -88,11 +85,84 @@ export function handleApprovalForAll(event: ApprovalForAll): void {
   // - contract.uri(...)
 }
 
-export function handleDeLinked(event: DeLinked): void {}
+export function handleDeLinked(event: DeLinked): void {
+  let link = Link.load(event.params.linkId_.toHex());
+  if (link) {
+    link.weight = link.weight.minus(event.params.weight);
+    link.save();
+  }
+}
 
-export function handleLinked(event: Linked): void {}
+export function handleLinked(event: Linked): void {
+  let subject = NFT.load(event.params.subjectLinkerId_.toHex());
+  let object = NFT.load(event.params.objectLinkerId_.toHex());
 
-export function handleLinkerMinted(event: LinkerMinted): void {}
+  if (!subject) {
+    subject = new NFT(event.params.subjectLinkerId_.toHex());
+    subject.chainId = event.params.subjectChainId_;
+    subject.address = event.params.subjectTokenAddress_.toHexString();
+    subject.tokenId = event.params.subjectTokenId_;
+    subject.balance = BigInt.fromI32(0);
+    subject.value = BigInt.fromI32(0);
+    if (object) {
+      subject.points = object.points.div(BigInt.fromI32(10));
+    }
+    subject.totalSupply = BigInt.fromI32(0);
+  } else {
+    if (object) {
+      subject.points = subject.points.plus(
+        object.points.div(BigInt.fromI32(10))
+      );
+    }
+  }
+
+  subject.save();
+
+  let link = Link.load(event.params.linkId_.toHex());
+  if (!link) {
+    link = new Link(event.params.linkId_.toHex());
+    link.subject = event.params.subjectLinkerId_.toHex();
+    link.target = event.params.objectLinkerId_.toHex();
+    link.weight = event.params.weight;
+  } else {
+    link.weight = link.weight.plus(event.params.weight);
+  }
+  link.save();
+}
+
+export function handleLinkerMinted(event: LinkerMinted): void {
+  /*
+    event LinkerMinted(
+        uint256 indexed linkerId_,
+        uint256 chainId_,
+        address tokenAddress_,
+        uint256 tokenId_,
+        uint256 mintPrice_,
+        uint256 systemShare_,
+        uint256 referrerShare_,
+        address referrer,
+        uint256 tokenShare_
+    );
+  */
+  let nft = NFT.load(event.params.linkerId_.toHex());
+
+  if (!nft) {
+    nft = new NFT(event.params.linkerId_.toHex());
+    nft.chainId = event.params.chainId_;
+    nft.address = event.params.tokenAddress_.toHexString();
+    nft.tokenId = event.params.tokenId_;
+    nft.balance = event.params.tokenShare_;
+    nft.value = event.params.mintPrice_;
+    nft.points = event.params.mintPrice_;
+    nft.totalSupply = BigInt.fromI32(1);
+  } else {
+    nft.balance = nft.balance.plus(event.params.tokenShare_);
+    nft.value = event.params.mintPrice_;
+    nft.points = event.params.mintPrice_.plus(event.params.mintPrice_);
+    nft.totalSupply = nft.totalSupply.plus(BigInt.fromI32(1));
+  }
+  nft.save();
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
